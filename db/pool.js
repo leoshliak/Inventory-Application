f3a778ce9602785442d12e971b70b357349fe8e5
leoshliak <levshlyak@gmail.com>
2025-07-04 17:26:46 +0300

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -6,5 +6,6 @@ module.exports = new Pool({
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT // The default port
+    port: process.env.DB_PORT, // The default port
+    ssl: { rejectUnauthorized: false }
 })
